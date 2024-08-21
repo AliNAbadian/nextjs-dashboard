d1246b4430159d4655deb416f782cc56390d5760
AliNAbadian <nagshrizali@gmail.com>
2024-08-21 06:38:37 -0700

    "Refactored dashboard page to use suspense and skeletons, removed prop from LatestInvoices component, and updated imports and function calls for data fetching."

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,11 +3,12 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { inter, lusitana } from "@/app/ui/fonts";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
-import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton, } from "@/app/ui/skeletons";
+import { fetchCardData } from "@/app/lib/data";
+
 
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -35,7 +36,13 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        <Suspense fallback={<CardsSkeleton />}>
+
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
